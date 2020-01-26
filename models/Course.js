fdb85b60988875ef1454d087e1055a194bf2b2c1
Ken Klooster <kklooster3910@gmail.com>
2020-01-26 11:18:28 -0500

    new branch fam - course model/info

--- models/Course.js
+++ models/Course.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+// adding comment for ssh key test
+
 const CourseSchema = new Schema({});
 
 module.exports = Course = mongoose.model("Course", CourseSchema);
