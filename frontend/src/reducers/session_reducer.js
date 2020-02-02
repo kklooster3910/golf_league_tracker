9f154592a975ce3997068207da8d0c44367c582f
Ken Klooster <kklooster3910@gmail.com>
2020-02-02 08:28:42 -0500

    fix: cleanup

--- frontend/src/reducers/session_reducer.js
+++ frontend/src/reducers/session_reducer.js
@@ -26,3 +26,6 @@ export default (state = initialState, { type, currentUser }) => {
       return state;
   }
 };
+
+//adding a comment for GIT ssh  prurpose --- hopefully I don't have to login every time I
+// make a push now
